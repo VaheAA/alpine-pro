daa4454a0520ae163e95e1e2d1b871027ddeefa2
vahe abovyan <vahe.abov@gmail.com>
2023-11-23 21:48:59 +0400

    first commit

---
+++ src/assets/js/index.js
@@ -0,0 +1,9 @@
+import '../../index.html';
+import '../scss/style.scss';
+import './modules/mobileMenu';
+import * as bootstrap from 'bootstrap';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ src/assets/js/modules/mobileMenu.js
@@ -0,0 +1,16 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuBtn = document.querySelector('#menu-btn');
+const body = document.body;
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('open');
+  mobileMenu.classList.toggle('open');
+  body.classList.toggle('no-scroll');
+});
+
+mobileMenu.addEventListener('click', (e) => {
+  if (e.target.classList.contains('form-control')) return;
+  menuBtn.classList.remove('open');
+  mobileMenu.classList.remove('open');
+  body.classList.remove('no-scroll');
+});
