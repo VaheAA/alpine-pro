c7e2f8c80a74934d8a7457996edccfabf44f9286
vahe abovyan <vahe.abov@gmail.com>
2023-11-25 19:54:19 +0400

    finish catalogue page

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,9 +2,13 @@ import '../../index.html';
 import '../scss/style.scss';
 import './modules/mobileMenu';
 import './modules/slider';
+import './modules/rangeSlider';
+import catalogue from './modules/catalogue';
 import * as bootstrap from 'bootstrap';
 
 import Alpine from 'alpinejs';
+
+Alpine.data('catalogue', catalogue);
 
 window.Alpine = Alpine;
 
---
+++ src/assets/js/modules/catalogue.js
@@ -0,0 +1,74 @@
+export default () => ({
+  colors: [
+    '#000000',
+    '#00FF38',
+    '#0029FF',
+    '#FFFFFF',
+    '#285DA1',
+    '#2B2838',
+    '#FF0000',
+  ],
+  sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+  catalogue: [
+    {
+      id: 1,
+      title: 'Product title can be quite long',
+      price: '$ 45,880',
+      img: require('../../img/content/products/product-4.png'),
+    },
+    {
+      id: 2,
+      title: 'Product title can be quite long',
+      price: '$ 45,880',
+      img: require('../../img/content/products/product-3.png'),
+    },
+    {
+      id: 3,
+      title: 'Product title can be quite long',
+      price: '$ 45,880',
+      img: require('../../img/content/products/product-2.png'),
+    },
+    {
+      id: 4,
+      title: 'Product title can be quite long',
+      price: '$ 45,880',
+      img: require('../../img/content/products/product-4.png'),
+    },
+    {
+      id: 5,
+      title: 'Product title can be quite long',
+      price: '$ 45,880',
+      img: require('../../img/content/products/product-3.png'),
+    },
+    {
+      id: 6,
+      title: 'Product title can be quite long',
+      price: '$ 45,880',
+      img: require('../../img/content/products/product-2.png'),
+    },
+    {
+      id: 7,
+      title: 'Product title can be quite long',
+      price: '$ 45,880',
+      img: require('../../img/content/products/product-2.png'),
+    },
+    {
+      id: 8,
+      title: 'Product title can be quite long',
+      price: '$ 45,880',
+      img: require('../../img/content/products/product-3.png'),
+    },
+    {
+      id: 9,
+      title: 'Product title can be quite long',
+      price: '$ 45,880',
+      img: require('../../img/content/products/product-4.png'),
+    },
+    {
+      id: 10,
+      title: 'Product title can be quite long',
+      price: '$ 45,880',
+      img: require('../../img/content/products/product-4.png'),
+    },
+  ],
+});
